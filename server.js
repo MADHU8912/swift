e34fd28c8df640770fe712a842aa35542d3e3740
MADHU8912 <nikhilabba12@gmail.com>
2026-04-15 14:20:14 +0530

    add old version

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files
 app.use(express.static(__dirname));
 
-// Route for home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
